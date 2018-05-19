faeb698c59402910bff72afe5f9a3b765d56a03f
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2018-05-19 20:56:37 -0300

    First functional version for login

---
+++ myApp/controllers/authcontroller.js
@@ -0,0 +1,27 @@
+var exports = module.exports = {}
+
+exports.signup = function(req, res) {
+
+    res.render('login/login.ejs');
+
+}
+
+exports.signin = function(req, res) {
+    res.render('signin.ejs');
+}
+
+exports.dashboard = function(req, res) {
+
+    res.render('dashboard.ejs');
+
+}
+
+exports.logout = function(req, res) {
+
+    req.session.destroy(function(err) {
+
+        res.redirect('/');
+
+    });
+
+}
--- myApp/server.js
+++ myApp/server.js
@@ -1,28 +1,63 @@
 var express = require('express')
 var app = express()
-
 var passport = require('passport')
 var session = require('express-session')
 var bodyParser = require('body-parser')
-
 var env = require('dotenv').load()
+var exphbs = require('express-handlebars')
 
-// For bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
 
-// For passport
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+//For BodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+// For Passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
+
+
+//For Handlebars
+app.set('views', './views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+
+app.get('/', function(req, res) {
+
+    res.send('Welcome to Passport with Sequelize');
+
+});
 
 //Models
 var models = require("./models");
 
+//Routes
+
+var authRoute = require('./routes/auth.js')(app, passport);
+
+
+//load passport strategies
+
+require('./config/passport/passport.js')(passport, models.user);
+
+
 //Sync Database
+
 models.sequelize.sync().then(function() {
 
     console.log('Nice! Database looks fine')
+
 
 }).catch(function(err) {
 
@@ -30,13 +65,13 @@ models.sequelize.sync().then(function() {
 
 });
 
-app.get("/", function (req, res) {
-  res.render("login/login.ejs")
-})
 
-app.get("/login", function(req, res) {
-  var username = req.body.username
-  var password = req.body.pass
-})
+app.listen(3000, function(err) {
 
-app.listen(3000)
+    if (!err)
+
+        console.log("Site is live");
+
+    else console.log(err)
+
+});
