9acbde7bc8505e7fe2abce9de9c752df2593cf06
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2018-05-19 22:45:03 -0300

    Role added

---
+++ myApp/controllers/authcontroller.js
@@ -0,0 +1,41 @@
+var exports = module.exports = {}
+
+exports.presignup = function(req, res) {
+    res.render('presignup.ejs');
+}
+
+exports.signup = function(req, res) {
+    res.render('signout.ejs');
+}
+
+exports.signupAluno = function(req, res) {
+    res.render('signupAluno.ejs');
+}
+
+exports.signupCorretor = function(req, res) {
+    res.render('signupCorretor.ejs');
+}
+
+exports.signupCoordenador = function(req, res) {
+    res.render('signupCoordenador.ejs');
+}
+
+exports.signin = function(req, res) {
+    res.render('signin.ejs');
+}
+
+exports.dashboard = function(req, res) {
+
+    res.render('dashboard.ejs');
+
+}
+
+exports.logout = function(req, res) {
+
+    req.session.destroy(function(err) {
+
+        res.redirect('/');
+
+    });
+
+}
