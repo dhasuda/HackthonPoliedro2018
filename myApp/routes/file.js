3a113bceca8b37ab11e0b5fc18421648efdbdf46
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2018-05-20 05:04:01 -0300

    More features added to the back end

--- myApp/routes/file.js
+++ myApp/routes/file.js
@@ -101,4 +101,36 @@ module.exports = function(app, models) {
     });
   });
 
+  app.post('/uploadCompositionForStudent', function(req, res) {
+    if (!req.files)
+      return res.status(400).send('No files were uploaded.');
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+        filename = sampleFile.name;
+        console.log("############## USER RM = " + req.user.email)
+
+        filename = req.user.email + "&&" + req.body.week + "&&" + req.body.type + ".png";
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv('./compositions/' +  filename, function(err) {
+      if (err)
+        return res.status(500).send(err);
+
+        var data = {
+            studentEmail: req.body.studentRm,
+            type: req.body.type,
+            week: req.body.week,
+            archive: filename,
+            studentEmail: req.user.email,
+            about: req.user.about,
+            grade: -1
+        };
+
+      var Composition = models.composition
+      Composition.create(data)
+
+      res.send('File uploaded!');
+    });
+  });
+
 }
